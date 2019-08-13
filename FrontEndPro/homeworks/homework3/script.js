c405c15c784f407ba4a4e4a005b137afc1ca183d
masslou <aleksandr.olejnik@privatbank.ua>
2019-08-13 03:11:28 +0300

    added third homework

--- FrontEndPro/homeworks/homework3/script.js
+++ FrontEndPro/homeworks/homework3/script.js
@@ -1,19 +1,30 @@
 'use strict';
 
 let factor = null;
-askFactor();
-doMultiplication(factor);
-askRepeatProgram();
+
+startProgram();
+
+function startProgram() {
+    console.clear();
+    askFactor();
+    doMultiplication(factor);
+    askRepeatProgram()
+}
 
 function askFactor() {
+    const MIN_VALUE = 1;
+    const MAX_VALUE = 11;
 
     do {
         factor = +prompt('Enter the factor');
-    } while (isNaN(factor) && factor > 0 && factor < 11);
-
+    } while (isNaN(factor) &&
+        factor >= MIN_VALUE &&
+        factor < MAX_VALUE
+        );
 }
 
 function doMultiplication(factor) {
+
     const MIN_VALUE = 1;
     const MAX_VALUE = 11;
 
@@ -32,13 +43,5 @@ function printResult(result) {
 }
 
 function askRepeatProgram() {
-    do {
-        console.clear();
-        askFactor();
-        doMultiplication(factor);
-
-    } while (confirm('Repeat again'));
-
-    alert('Have a nice day!')
-
+    confirm('Repeat again?') ? startProgram() : alert('Have a nice day!');
 }
