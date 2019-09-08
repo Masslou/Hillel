7221d00ede426e9686084b5e70ee1f17eb4085c3
masslou <aleksandr.olejnik@privatbank.ua>
2019-09-08 17:48:47 +0300

    finished hw #10

--- FrontEndPro/classwork/classwork10/script.js
+++ FrontEndPro/classwork/classwork10/script.js
@@ -1,24 +1,26 @@
 'use strict';
 
-function calculator(value) {
-    let generalValue = value;
-
-    return {
-        add: (argument) => argument+generalValue,
-        multiple: (argument) => argument*generalValue,
-        divisor: (argument) => argument/generalValue,
-        sub: (argument) => argument-generalValue,
-        setValue: (argument) => generalValue = argument
-    }
-}
-
 
 const counter = calculator(10);
 
 
+function calculator(value) {
+    let generalValue = value;
+
+    return {
+        add: (argument) => argument + generalValue,
+        multiple: (argument) => argument * generalValue,
+        divide: (argument) => argument / generalValue,
+        sub: (argument) => argument - generalValue,
+        set: (argument) => generalValue = argument
+    }
+}
+
 console.log(counter.add(100)); //110
 console.log(counter.multiple(2));
-console.log(counter.divisor(2));
+console.log(counter.divide(2));
 console.log(counter.sub(10));
-console.log(counter.setValue(10));
+console.log(counter.set(10));
+
+
 
