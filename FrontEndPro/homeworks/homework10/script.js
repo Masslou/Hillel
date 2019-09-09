d5cdeddfcf962ab7cbd5f90cdc77acd4fb8ac424
masslou <aleksandr.olejnik@privatbank.ua>
2019-09-09 13:31:03 +0300

    some changes

--- FrontEndPro/homeworks/homework10/script.js
+++ FrontEndPro/homeworks/homework10/script.js
@@ -4,22 +4,23 @@
 const calculator = createCalculator(10);
 
 
-function createCalculator(value) {
-    let mainValue = value || 0;
+function createCalculator(value = 0) {
+    let mainValue = value;
+
     return {
-        add: (argument) => mainValue + argument,
-        multiply: (argument) => mainValue * argument,
-        divide: (argument) => mainValue / argument,
-        subtract: (argument) => mainValue - argument,
+        add: (argument) => mainValue += argument,
+        multiply: (argument) => mainValue *= argument,
+        divide: (argument) => mainValue /= argument,
+        subtract: (argument) => mainValue -= argument,
         set: (argument) => mainValue = argument
     }
 }
 
 console.log(calculator.add(100)); //110
-console.log(calculator.multiply(2)); // 20
-console.log(calculator.divide(2)); // 5
-console.log(calculator.subtract(10)); // 0
-console.log(calculator.set(70)); // 10
+console.log(calculator.multiply(2)); // 220
+console.log(calculator.divide(2)); // 110
+console.log(calculator.subtract(10)); // 100
+console.log(calculator.set(70)); // 70
 console.log(calculator.add(10)); // 80
 
 
