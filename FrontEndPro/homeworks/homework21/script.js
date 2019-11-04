be624f88794a93ba5c584cef381e5e6dec8de4ce
masslou <aleksandr.olejnik@privatbank.ua>
2019-11-04 16:37:40 +0200

    hw

---
+++ FrontEndPro/homeworks/homework21/script.js
@@ -0,0 +1,110 @@
+$(function () {
+
+    const DELETE_STICKER_CLASS = 'delete-sticker-btn';
+    const STICKER_ITEM_CLASS = 'stickers-container-item';
+
+    const stickerTemplate = $('#stickersTemplate').html();
+    const $modalWindow = $('#modal-window');
+    const $stickersContainer = $('#stickers-container');
+    const $addStickerButton = $('#add-sticker-button');
+    const dialog = $modalWindow.dialog({
+        autoOpen: false,
+        height: 400,
+        width: 350,
+        modal: true,
+        buttons: {
+            'Create an sticker': addSticker,
+            Cancel: function () {
+                dialog.dialog('close');
+            }
+        },
+        close: function () {
+            form[0].reset();
+        }
+    });
+
+    let stickersList = [];
+    const form = dialog.find('form').on('submit', onFormSubmit);
+    $stickersContainer.on('click', `.${DELETE_STICKER_CLASS}`, onDeleteBtnClick);
+
+
+    init();
+
+    function init() {
+        stickersList = getState();
+        renderList(stickersList);
+    }
+
+    function onFormSubmit(event) {
+        event.preventDefault();
+        addSticker();
+    }
+
+    $addStickerButton.on('click', () => {
+        dialog.dialog('open');
+    });
+
+
+    function addSticker() {
+        let newSticker = {};
+        form.serializeArray().forEach(el => newSticker[el.name] = el.value);
+        newSticker.id = Date.now();
+        stickersList.push(newSticker);
+        renderList(stickersList);
+        saveState();
+
+        dialog.dialog('close');
+    }
+
+
+    function onDeleteBtnClick() {
+        const deletSticker = $(this).closest(`.${STICKER_ITEM_CLASS}`);
+        const stickerID = deletSticker.data('stickerId');
+
+        deletSticker.remove();
+        deleteSticker(stickerID);
+    }
+
+
+    function saveState() {
+        localStorage.setItem('stickers', JSON.stringify(stickersList));
+    }
+
+
+    function getState() {
+        const data = localStorage.getItem('stickers');
+        return data ? JSON.parse(data) : [];
+    }
+
+
+    function render() {
+        renderList(stickersList);
+        saveState();
+    }
+
+    function renderList(list) {
+        const containerItemsHtml = list.map((sticker) => generateSticker(sticker));
+        $stickersContainer.html(containerItemsHtml.join(''));
+    }
+
+    function generateSticker(sticker) {
+        return stickerTemplate.replace('{{id}}', sticker.id)
+            .replace('{{title}}', sticker.title)
+            .replace('{{description}}', sticker.description);
+    }
+
+    function deleteSticker(stickerID) {
+        stickersList = stickersList.filter(el => el.id != stickerID);
+        deleteStickerItemElement(stickerID);
+        saveState();
+    }
+
+    function deleteStickerItemElement(id) {
+        const element = getStickerItemElement(id);
+        element && element.remove();
+    }
+
+    function getStickerItemElement(id) {
+        return $(`[data-sticker-id="${id}"]`);
+    }
+});
--- FrontEndPro/homeworks/homework22/script.js
+++ FrontEndPro/homeworks/homework22/script.js
@@ -1,109 +1,41 @@
 $(function () {
+    const usersUrl = 'https://api.github.com/search/users?q=';
+    searchUser(usersUrl);
+});
 
-    const DELETE_STICKER_CLASS = 'delete-sticker-btn';
-    const STICKER_ITEM_CLASS = 'stickers-container-item';
-
-    let stickersList = [];
-    const stickerTemplate = $('#stickersTemplate').html();
-    const $modalWindow = $('#modal-window');
-    const $stickersContainer = $('#stickers-container');
-    const $addStickerButton = $('#add-sticker-button');
-    const dialog = $modalWindow.dialog({
-        autoOpen: false,
-        height: 400,
-        width: 350,
-        modal: true,
-        buttons: {
-            'Create an sticker': addSticker,
-            Cancel: function () {
-                dialog.dialog('close');
-            }
+function searchUser(url) {
+    $(".search-user-input").autocomplete({
+        minLength: 2,
+        messages: {
+            noResults: 'no results',
         },
-        close: function () {
-            form[0].reset();
+            select: function (event, ui) {
+                fetch(ui.item.url).then(response => {
+                    return response.json()
+                }).then(data => {
+                    renderUserDetails(generateUserDetails(data));
+                })
+        },
+
+        source: function (request, response) {
+            $.ajax({
+                url: `${url}${request.term}`,
+                success: function (result) {
+                    const gitHubResponse = result.items.map((item) => {
+                        return {label: item.login, data: item}
+                    });
+                    response(gitHubResponse);
+                }
+            });
         }
     });
+}
 
-    const form = dialog.find('form').on('submit', onFormSubmit);
-    $stickersContainer.on('click', `.${DELETE_STICKER_CLASS}`, onDeleteBtnClick);
+function userInformationRender() {
 
-    function onFormSubmit(event) {
-        event.preventDefault();
-        addSticker();
-    }
+}
 
-    $addStickerButton.on('click', function () {
-        dialog.dialog('open');
-    });
+function generateDate(date) {
+    date = new Date();
 
-    init();
-
-    function init() {
-        stickersList = getState();
-        renderList(stickersList);
-    }
-
-
-    function addSticker() {
-        let newSticker = {};
-        form.serializeArray().forEach(el => newSticker[el.name] = el.value);
-        newSticker.id = Date.now();
-        stickersList.push(newSticker);
-        renderList(stickersList);
-        saveState();
-
-        dialog.dialog('close');
-    }
-
-
-    function onDeleteBtnClick() {
-        const deletSticker = $(this).closest(`.${STICKER_ITEM_CLASS}`);
-        const stickerID = deletSticker.data('stickerId');
-
-        deletSticker.remove();
-        deleteSticker(stickerID);
-    }
-
-
-    function saveState() {
-        localStorage.setItem('stickers', JSON.stringify(stickersList));
-    }
-
-
-    function getState() {
-        const data = localStorage.getItem('stickers');
-        return data ? JSON.parse(data) : [];
-    }
-
-
-    function render() {
-        renderList(stickersList);
-        saveState();
-    }
-
-    function renderList(list) {
-        const containerItemsHtml = list.map((sticker) => generateSticker(sticker));
-        $stickersContainer.html(containerItemsHtml.join(''));
-    }
-
-    function generateSticker(sticker) {
-        return stickerTemplate.replace('{{id}}', sticker.id)
-            .replace('{{title}}', sticker.title)
-            .replace('{{description}}', sticker.description);
-    }
-
-    function deleteSticker(stickerID) {
-        stickersList = stickersList.filter(el => el.id != stickerID);
-        deleteStickerItemElement(stickerID);
-        saveState();
-    }
-
-    function deleteStickerItemElement(id) {
-        const element = getStickerItemElement(id);
-        element && element.remove();
-    }
-
-    function getStickerItemElement(id) {
-        return $(`[data-sticker-id="${id}"]`);
-    }
-});
+}
